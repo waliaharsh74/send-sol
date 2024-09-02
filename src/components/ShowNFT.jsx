9bed542447021aa911b7578b6d57893a8405cbaf
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2024-09-02 15:35:30 +0530

    Update ShowNFT.jsx
    Some UX changes

--- src/components/ShowNFT.jsx
+++ src/components/ShowNFT.jsx
@@ -10,6 +10,8 @@ function ShowNFT() {
     const { templateId } = useParams();
     const [nft, setNft] = useState({});
     const [loading, setLoading] = useState(true);
+    const [msg, setMsg] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
     const [metaData, setMetaData] = useState({});
 
     useEffect(() => {
@@ -31,6 +33,11 @@ function ShowNFT() {
     }, [templateId]);
 
     const handleMintNft = async () => {
+
+        setShowPopup(true);
+            setTimeout(() => {
+                setShowPopup(false);
+            }, 5000);
         try {
             const body = {
                 collectionId: "d383cf76-23f5-4c1e-a97e-b9f6e9e0b112",
@@ -39,10 +46,16 @@ function ShowNFT() {
                 templateId
             };
             const { data } = await axios.post(`https://nft-backend-a3ah.onrender.com/wesol/v1/mint-nft`, body);
+            
             console.log(data);
             setMetaData(data);
+            
         } catch (error) {
             console.log(error);
+            setShowPopup(true);
+            setTimeout(() => {
+                setShowPopup(false);
+            }, 5000);
         }
     };
 
@@ -50,7 +63,10 @@ function ShowNFT() {
         <div className="full-container">
             <h1 className="title">Mint NFT</h1>
             {loading ? (
+                <div>
+                <h2 className="warning">Please wait requests may take 50 seconds or more!</h2>
                 <ShimmerPlaceholder count={6} />
+            </div>
             ) : (
                 <div>
                     <div className="image-container">
@@ -70,11 +86,14 @@ function ShowNFT() {
                             Mint NFT
                         </button>
                     )}
-                    {(metaData?.status || metaData?.onChain) && (
+                    {(metaData?.status || metaData?.onChain) ? (
                         <div className="status">
                             <div>Status: {metaData?.status || metaData?.onChain?.status}</div>
                             <div>Message: {metaData?.msg || "Minting Started"}</div>
                         </div>
+                    ):(
+                        showPopup && (
+                            <div className='popup'>Minting NFT to your Wallet has been Started!</div>)
                     )}
                 </div>
             )}
@@ -83,3 +102,4 @@ function ShowNFT() {
 }
 
 export default ShowNFT;
+
