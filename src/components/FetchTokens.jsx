cc12f7366770f22a1630cfafd9ce42e313899d80
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2024-09-13 00:48:41 +0530

    Update FetchTokens.jsx

--- src/components/FetchTokens.jsx
+++ src/components/FetchTokens.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
-import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import axios from 'axios';
 import './FetchTokens.css';
-import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
-import { Metaplex } from '@metaplex-foundation/js';
-import { programs } from "@metaplex/js"
-
 
 const rpcEndpoint = 'https://api.devnet.solana.com';
 
@@ -92,30 +89,64 @@ const FetchTokens = ({ walletToQuery }) => {
                         };
                     })
                 );
-                const allAccounts = await solanaConnection.getProgramAccounts(TOKEN_2022_PROGRAM_ID, {
-                    commitment: 'confirmed',
-                    filters: [
+                const allNonTokens = await axios.post("https://explorer-api.devnet.solana.com", {
+                    jsonrpc: "2.0",
+                    id: 1,
+                    method: "getTokenAccountsByOwner",
+                    params: [
+                        walletToQuery,
                         {
-                            memcmp: {
-                                offset: 32,
-                                bytes: new PublicKey(walletToQuery).toString(),
-                            },
+                            programId: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
                         },
-                    ],
-                });
-                console.log(allAccounts[0].pubkey.toString());
-                const metaplex = Metaplex.make(solanaConnection);
-                const metadataPda = metaplex.nfts().pdas().metadata({ mint: allAccounts[0].pubkey });
+                        {
+                            encoding: "jsonParsed"
+                        }
+                    ]
+                })
+                const nonTokenDataArr = allNonTokens?.data?.result?.value?.map((item) => {
 
+                    return {
+                        address: item.account.data.parsed.info.mint,
+                        metadata: null,
+                        tokenBalance:item.account.data.parsed.info.tokenAmount.uiAmountString
+                    }
 
+                })
+                console.log(nonTokenDataArr)
+                //   console.log(nonTokenDataArr)
+                const allSplTokens = await axios.post("https://explorer-api.devnet.solana.com", {
+                    jsonrpc: "2.0",
+                    id: 2,
+                    method: "getTokenAccountsByOwner",
+                    params: [
+                        walletToQuery,
+                        {
+                            programId: "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"
+                        },
+                        {
+                            encoding: "jsonParsed"
+                        }
+                    ]
+                })
+                const tokenMetaDataPromises = await allSplTokens.data.result.value?.map(async (item) => {
+                    const tokenMetaData = await fetchMetaData(item.account.data.parsed.info.mint)
+                    return {
+                        address: item.account.data.parsed.info.mint,
+                        metadata: tokenMetaData,
+                        tokenBalance:item.account.data.parsed.info.tokenAmount.uiAmountString
+                    }
 
-
-
+                })
+                const tokenMetaDataArray = await Promise.all(tokenMetaDataPromises);
+                // console.log(tokenMetaDataArray)
+                // console.log(tokenMetaDataArray);
+                setSplTokenAccounts(tokenMetaDataArray)
+                setTokenAccounts(nonTokenDataArr)
 
 
 
             } catch (err) {
-                console.error(err);
+                console.error(err)
                 setError(err.message);
             } finally {
                 setLoading(false);
@@ -136,18 +167,18 @@ const FetchTokens = ({ walletToQuery }) => {
                 <div className="error">Error: {error}</div>
             ) : (
                 <div className="token-accounts-list">
-                    {splTokenAccounts.length === 0 && tokenAccounts.length === 0 ? (
+                    {splTokenAccounts.length === 0 && tokenAccounts.length===0 ? (
                         <h1 className='nothing-found'>No token accounts found!</h1>
                     ) : (
                         <ul className='ul'>
-                            {splTokenAccounts.length > 0 && splTokenAccounts.map((account, i) => (
+                            {splTokenAccounts.length>0 && splTokenAccounts.map((account, i) => (
                                 <li key={i} className="token-account-item" >
                                     <div className="token-account-box">
                                         <div className='token-head'>
 
                                             <img src={account?.metadata ? account?.metadata.image : "unknown.jpg"} alt="unknown" />
                                             <div className='token-info'>
-                                                <h2 className='token-no'>{account?.metadata ? account?.metadata?.Symbol?.name : "Unknown Token "} </h2>
+                                    <h2 className='token-no'>{account?.metadata ? account?.metadata?.Symbol?.name : "Unknown Token "} </h2>
                                                 <div className='strong'>
                                                     <strong className='supply'>{account.tokenBalance}<span className='token-data'> {account?.metadata ? account?.metadata?.Symbol?.symbol : account?.address.substring(0, 7) + "..."}</span></strong>
                                                 </div>
@@ -161,7 +192,7 @@ const FetchTokens = ({ walletToQuery }) => {
                                     </div>
                                 </li>
                             ))}
-                            {tokenAccounts.length > 0 && tokenAccounts.map((account, i) => (
+                            {tokenAccounts.length>0 && tokenAccounts.map((account, i) => (
                                 <li key={i} className="token-account-item" >
                                     <div className="token-account-box">
                                         <div className='token-head'>
